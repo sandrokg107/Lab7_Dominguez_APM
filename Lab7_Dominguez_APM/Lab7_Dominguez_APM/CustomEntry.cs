8b077474d3244ad7b3acc5e0a55cbe8fff4ac281
Alumno <Alumno@LAB1509-12>
2023-10-20 15:13:10 -0500

    Agregar archivos de proyecto.

---
+++ Lab7_Dominguez_APM/Lab7_Dominguez_APM/CustomEntry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Lab7_Dominguez_APM
+{
+    public class CustomEntry : Entry
+    {
+  
+    }
+}
