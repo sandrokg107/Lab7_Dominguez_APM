3cbd5f28ff1895831b5fd792aa633f0dcbdb831d
sandrokg107 <sandrokg107@gmai.com>
2023-10-21 21:37:18 -0500

    ultimo git

--- Lab7_Dominguez_APM/Lab7_Dominguez_APM/MainPage.xaml.cs
+++ Lab7_Dominguez_APM/Lab7_Dominguez_APM/MainPage.xaml.cs
@@ -13,6 +13,38 @@ namespace Lab7_Dominguez_APM
         public MainPage()
         {
             InitializeComponent();
+            DynamicDemo.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new DynamicDemo());
+            };
+            ExplicitControlDemo.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ExplicitControlDemo());
+            };
+            ExplicitDemo.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ExplicitDemo());
+            };
+            GlobalDemo.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new GlobalDemo());
+            };
+            ImplicitControlDemo.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ImplicitControlDemo());
+            };
+            ImplicitDemo.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ImplicitDemo());
+            };
+            InheritanceDemo.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new InheritanceDemo());
+            };
+            StylesDemo.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new StylesDemo());
+            };
         }
     }
 }
